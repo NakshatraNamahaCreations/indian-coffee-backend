9261cec2531c77b69fa2a3411858812f17bb56f7
Nakshatra Namaha Creations <support@vijayhomeservices.com>
2026-02-06 14:43:46 +0530

    new changes

--- Route/Bid.js
+++ Route/Bid.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const bidCtrl = require("../Controller/Bid"); 
+const bidCtrl = require("../Controller/Bid");
 
 router.post("/lock-after-payment", bidCtrl.lockAfterPayment);
-router.post("/createbid", bidCtrl.createBid);             
-router.get("/lock/:productId", bidCtrl.getLockStatus);       
+router.post("/createbid", bidCtrl.createBid);
+router.get("/lock/:productId", bidCtrl.getLockStatus);
 router.get("/user/:userId/product/:productId", bidCtrl.getUserBidsForProduct);
 router.get("/all", bidCtrl.getAllBids);
 router.patch("/vendor-accept/:id", bidCtrl.vendorAcceptBid);
+router.put("/vendor-reject/:id", bidCtrl.vendorRejectBid);
 router.patch("/admin-approve/:id", bidCtrl.adminApproveBid);
 router.patch("/reject/:id", bidCtrl.adminrejectBid);
 router.get('/user/:userId', bidCtrl.getBidsByUser);
 router.get("/vendordata/:vendorId", bidCtrl.getBidsByVendor);
-
 
 module.exports = router;
